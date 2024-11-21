4eb0c2d699cfa1bdd54a809a8fc71387e0927230
Alec Ebert <aebe9584@gmail.com>
2024-11-21 16:51:38 -0700

    included skeleton, changed event to int

--- Tile.h
+++ Tile.h
@@ -1,6 +1,10 @@
+/* The Tile structure contains two variables, one for color and one for event.
+ - Tile type varies based on the numeric value of event.
+*/
+
 #define TILE_H
 
 struct Tile{
     char color;
-    char event;
+    int event;
 };
