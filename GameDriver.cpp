bfc3a2ced34d9c0139e865e5684ee87fa62a8b49
Alec Ebert <aebe9584@gmail.com>
2024-12-11 16:39:12 -0700

    changed num events and riddles

--- GameDriver.cpp
+++ GameDriver.cpp
@@ -22,8 +22,8 @@ int main(){
 
     gm.characterCreation(players, NUM_PLAYERS, advisors, NUM_ADVISORS);
 
-    const int NUM_EVENTS = 6;
-    const int NUM_RIDDLES = 3;
+    const int NUM_EVENTS = 19;
+    const int NUM_RIDDLES = 11;
 
     string events[NUM_EVENTS][10];
     string riddles[NUM_RIDDLES][10];
